f1d3e1be5496ef9a23731040055a11ffe4274018
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2020-05-20 21:17:44 +0100

    nav icon, colors, data structure

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Nav from './components/Nav'
 function App() {
   return (
     <Router>
-      {/* <Nav /> */}
       <Switch>
         <Route path="/">
           <Facts />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,20 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCat } from '@fortawesome/free-solid-svg-icons'
 
 function Nav() {
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        <a className="navbar-item" href="/"><h4 className="title is-4">Cat Facts</h4></a>
+        <a className="navbar-item" href="/"><h4 className="title is-4"><FontAwesomeIcon icon={faCat} /> Cat Facts</h4></a>
       </div>
 
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <a className="navbar-item">Home</a>
-        </div>
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-primary">
-                <strong>Add a fact</strong>
-              </a>
-            </div>
+      <div className="navbar-end">
+        <div className="navbar-item">
+          <div className="buttons">
+            <a className="button is-primary">
+              <strong>Add a fact</strong>
+            </a>
           </div>
         </div>
       </div>
