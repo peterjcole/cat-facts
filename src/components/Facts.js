a54e2cf33f7f2070ebc8ab026691f043113365fc
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2020-05-22 20:26:45 +0100

    get more facts if we need more facts

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -9,46 +9,49 @@ const today = new Date() //todo store in ref
 function Facts() {
 
   const [facts, setFacts] = useState([])
-  const [cursor, setCursor] = useState({})
+  const [cursor, setCursor] = useState(0)
 
   useEffect(() => {
     if (facts.length === 0) {
-      updateFacts()
+      getMoreFacts()
     }
   }, [])
 
-  const updateFacts = () => {
+  const getMoreFacts = () => {
     const query = firebase.firestore().collection('facts')
       .where('approved', '==', true)
       .where('displayDate', '<=', today)
       .orderBy('displayDate', 'desc')
       .limit(10)
 
-    query.get().then(facts => {
-      setFacts(facts.docs)
-      setCursor(0)
+    const fullQuery = facts.length ? query.startAfter(facts[facts.length - 1]) : query
+
+    fullQuery.get().then(newFacts => {
+      setFacts((oldFacts) => {
+        return oldFacts.concat(newFacts.docs)
+      })
     })
   }
 
   const goOlder = () => {
-    if (cursor < facts.length - 1) {
+    if (cursor === facts.length - 2) {
+      getMoreFacts()
+    }
+
+    if (!isOldestFact()) {
       setCursor(cursor + 1)
     }
   }
 
   const goNewer = () => {
-    if (cursor > 0) {
+    if (!isNewestFact()) {
       setCursor(cursor - 1)
     }
   }
 
-  const addFact = () => {
-    firebase.firestore().collection('facts').add({
-      text: "This is an interesting fact" + Math.random(),
-      submitted: today,
-      approved: true
-    })
-  }
+  const isOldestFact = () => cursor === facts.length - 1
+
+  const isNewestFact = () => cursor === 0
 
   return (
     <section className="hero is-fullheight">
@@ -57,12 +60,13 @@ function Facts() {
         <div className="container">
           <div className="columns is-vcentered">
             <div className="column">
-              <h1 className="title" onClick={goOlder}>{"<"}</h1>
+              {!isOldestFact() && <h1 className="title" onClick={goOlder}>{"<"}</h1>}
             </div>
-            {facts[cursor] && <Fact data={facts[cursor].data()} />}
-            {/* <button className="button" onClick={addFact}>Add fact</button> */}
+            {facts[cursor] &&
+              <Fact data={facts[cursor].data()} />
+            }
             <div className="column">
-              <h1 className="title" onClick={goNewer}>{">"}</h1>
+              {!isNewestFact() && <h1 className="title" onClick={goNewer}>{">"}</h1>}
             </div>
           </div>
         </div>
@@ -72,7 +76,7 @@ function Facts() {
 }
 
 function Fact(props) {
-  const {displayDate, text} = props.data
+  const { displayDate, text } = props.data
   return (
     <div className="column is-four-fifths">
       <h1 className="title">{text}</h1>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,13 +8,14 @@ function Nav() {
       <div className="navbar-brand">
         <a className="navbar-item" href="/"><h4 className="title is-4"><FontAwesomeIcon icon={faCat} color="#e76f51"/> Cat Facts</h4></a>
       </div>
-
-      <div className="navbar-end">
-        <div className="navbar-item">
-          <div className="buttons">
-            <a className="button is-primary">
-              <strong>Add a fact</strong>
-            </a>
+      <div className="navbar-menu is-active">
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Add a fact</strong>
+              </a>
+            </div>
           </div>
         </div>
       </div>
